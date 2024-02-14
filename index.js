b5a5317d0dadfa1a207f1c114ef3c3b7ec555588
Jessica <76233443+jessicamarie7@users.noreply.github.com>
2024-02-13 22:05:39 -0600

    Add files via upload

--- index.js
+++ index.js
@@ -1,66 +1,40 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
-import { getDatabase, ref, onValue, push, remove } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js";
+import { getDatabase, ref, push, onValue, remove } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js";
 
 const appSettings = {
     databaseURL: "https://endorsements-f182c-default-rtdb.firebaseio.com/"
 }
 
-const app = initializeApp(appSettings)
-const database = getDatabase(app)
-const endorsementRef = ref(database, "Endorsements")
+const app = initializeApp(appSettings);
+const database = getDatabase(app);
+const inputEl = document.getElementById("endorsement-input");
+const buttonEl = document.getElementById("publish-button");
+const endorsementsEl = document.getElementById("endorsements-container");
+const endorsementsInDb = ref(database, "Endorsements");
 
-onValue(endorsementRef, function (snapshot) {
+buttonEl.addEventListener("click", function () {
+    let newEndorsement = inputEl.value;
+    push(endorsementsInDb, newEndorsement);
+})
+
+function appendEndorsement(newItem) {
+    let itemIndex = newItem[0];
+    let itemValue = newItem[1];
+    let endorsementToDom = document.createElement("p");
+    endorsementToDom.textContent = itemValue;
+    endorsementsEl.append(endorsementToDom);
+}
+
+
+
+onValue(endorsementsInDb, function (snapshot) {
     if (snapshot.exists()) {
         let endorsementsArray = Object.entries(snapshot.val())
-        for (let i = 0; i < endorsementArray.length; i++) {
-            let currentItem
-            appendEndorsementsRes(currentItem)
+        for (let i = 0; i < endorsementsArray.length; i++) {
+            let currentEndorsement = endorsementsArray[i];
+            appendEndorsement(currentEndorsement);
         }
+    } else {
+        endorsementsEl.innerHTML = "You have no endorsements yet 😔"
     }
 })
-
-function appendEndorsementsRes(resultText) {
-    let itemID = newItem[0];
-
-}
-
-function getEndorsementInput() {
-    let endorsementText = document.getElementById("endorsement-input").value;
-    return endorsementText;
-}
-
-let resultText = "";
-let endorsementArray = [];
-document.getElementById("publish-button").addEventListener("click", function () {
-    resultText = getEndorsementInput();
-    if (resultText === "") {
-        alert("Please enter something");
-    }
-    push(endorsementRef, {
-        endorsement: resultText
-    })
-    endorsementArray.push(resultText);
-    renderingEndorsements();
-})
-
-let endorsementsContainer = document.getElementById("endorsements-container")
-endorsementsContainer.innerHTML = "";
-
-function renderingEndorsements(newItem) {
-    let itemId = newItem[0];
-    let itemValue = newItem[1];
-    let endorsementEl = document.createElement("div");
-    endorsementEl = itemValue
-
-    endorsementEl.addEventListener("click", function () {
-        let exactLocationOfItemInDB = ref(database, `Endorsements/${itemId}`)
-
-        remove(exactLocationOfItemInDB)
-    })
-    endorsementsContainer.innerHTML = "";
-    for (let i = 0; i < endorsementArray.length; i++) {
-        endorsementEl.innerHTML = `<p>${endorsementArray[i]}</p>`;
-        endorsementsContainer.appendChild(endorsementEl);
-    }
-}
-
